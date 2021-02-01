a9c2bc42953e858d0b51e6ee5fb8fa6e7aa4e086
datajake1999 <datajake@gmail.com>
2021-02-01 02:49:02 -0800

    text2speak Is a member of CTTSEngObj.

--- ttsengobj.h
+++ ttsengobj.h
@@ -105,6 +105,7 @@ class ATL_NO_VTABLE CTTSEngObj :
     CComPtr<ISpObjectToken> m_cpToken;
     //ECI stuff
     ECIHand engine;
+char *text2speak;
 short buffer[4096];
 static ECICallbackReturn callback(ECIHand hEngine, enum ECIMessage Msg, long lParam, void *pData);
 
