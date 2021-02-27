77dcbe61ce1218ae128f9dc6b0e341ada5a31adb
datajake1999 <datajake@gmail.com>
2021-02-26 19:38:03 -0800

    Added install.bat.

---
+++ install.bat
@@ -0,0 +1,30 @@
+regsvr32 /s ttseng.dll
+regsvr32 /s enu.syn
+regsvr32 /s eng.syn
+regsvr32 /s esp.syn
+regsvr32 /s esm.syn
+regsvr32 /s fra.syn
+regsvr32 /s frc.syn
+regsvr32 /s deu.syn
+regsvr32 /s ita.syn
+regsvr32 /s chs.syn
+regsvr32 /s chsrom.dll
+regsvr32 /s ptb.syn
+regsvr32 /s jpn.syn
+regsvr32 /s jpnrom.dll
+regsvr32 /s fin.syn
+regsvr32 /s kor.syn
+regsvr32 /s korrom.dll
+reg import enu.reg /reg:32
+reg import eng.reg /reg:32
+reg import esp.reg /reg:32
+reg import esm.reg /reg:32
+reg import fra.reg /reg:32
+reg import frc.reg /reg:32
+reg import deu.reg /reg:32
+reg import ita.reg /reg:32
+reg import chs.reg /reg:32
+reg import ptb.reg /reg:32
+reg import jpn.reg /reg:32
+reg import fin.reg /reg:32
+reg import kor.reg /reg:32
